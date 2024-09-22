d1c783a71ea2d7d981aecf3af04ab4310140d6cb
qwerditya <50575098+miraclekombat@users.noreply.github.com>
2024-09-22 16:17:19 +0700

    Add files via upload
    Free source code web sederhana buat perpustakaan kelas online -cr MRzky (qwerditya)

---
+++ WebKelasFix/main.js
@@ -0,0 +1,111 @@
+const navCloseEl = document.querySelector('.nav__close');
+const navList = document.querySelector('.nav__list');
+const navIconEl = document.querySelector('.nav__icon');
+const navBgOverlayEl = document.querySelector('.nav__bgOverlay');
+
+
+
+
+window.addEventListener('DOMContentLoaded', () =>{
+  document.body.style.visibility = 'visible';
+});
+
+const navOpen = () => {
+   navList.classList.add('show');
+  navBgOverlayEl.classList.add('active');
+  document.body.style= 'visibility: visible; height: 100vh; width:100vw; overflow:hidden;';
+}
+
+const navClose = () => {
+  navList.classList.remove('show');
+  navBgOverlayEl.classList.remove('active');
+  document.body.style= 'visibility: visible; height: initial; width: 100%; overflow-x: hidden;';
+}
+
+    var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    /* Toggle between adding and removing the "active" class,
+    to highlight the button that controls the panel */
+    this.classList.toggle("active");
+
+    /* Toggle between hiding and showing the active panel */
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
+
+
+navIconEl.addEventListener('click', navOpen);
+
+navCloseEl.addEventListener('click', navClose);
+
+navBgOverlayEl.addEventListener('click', navClose)
+
+// AOS
+// AOS.refreshHard();
+AOS.init({
+  offset: 200, // offset (in px) from the original trigger point
+  delay: 100, // values from 0 to 3000, with step 50ms
+  duration: 400, // values from 0 to 3000, with step 50ms
+  easing: 'ease', // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+});
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+function showNotification() {
+  var notification = document.getElementById('notification');
+  notification.style.display = 'display';
+
+  // You can add additional logic or set a timeout to hide the notification after some time
+  setTimeout(function () {
+      notification.style.display = 'display';
+  }, 5000); // Hide after 10 seconds
+}
+
+// Call the function to show the notification
+showNotification();
+
+// const dishGridEl = Array.from(document.querySelectorAll('#dishGrid'));
+// if (dishGridEl.length > 0){
+//   // console.log(dishGridEl)
+//   dishGridEl.forEach(item => {
+//     item.setAttribute('data-aos', 'fade-up');
+//   })
+// }
